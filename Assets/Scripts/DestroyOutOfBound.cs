10cbadb065042a34de26f2ac096d3b2e6aedf322
Aaryan Bista <aaryanbista31@gmail.com>
2026-01-04 10:19:39 +0545

    first commit

---
+++ Assets/Scripts/DestroyOutOfBound.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class DestroyOutOfBound : MonoBehaviour
+{
+    private float topBound = 30.0f;
+    private float lowerBound = -10.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy the object if it goes out of bounds
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+
+    }
+}
